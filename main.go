f505679e928db5e740bb6585c65ca0b27138fd96
exwn <ixwanull.4za@outlook.com>
2022-10-07 09:11:25 +0700

    fix: updating fizzbuzz condition

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import "fmt"
 func main() {
 
 	input := 15
+
 	for i := 1; i <= input; i++ {
-		fmt.Println(i)
-		if i%3 == 0 {
-			fmt.Println(i, "Fizz")
-		}
-		if i%5 == 0 {
-			fmt.Println(i, "Buzz")
-		}
+
 		if i%15 == 0 {
-			fmt.Println(i, "FizzBuzz")
+			fmt.Println("FizzBuzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
 		}
 	}
 }
